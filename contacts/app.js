5d9e473ac06ddb539cdd1a7d647960cc2d1ec968
Cendey <cendey@yeah.net>
2017-01-09 17:01:46 +0800

    Revise request path prefix.

--- contacts/app.js
+++ contacts/app.js
@@ -25,7 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
-app.use('/contact', contacts);
+app.use('/contacts', contacts);
 
 // catch 404 and forward to error handler
 app.use(function (request, response, next) {
